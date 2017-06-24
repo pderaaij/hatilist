797c889a0694d3ce27926998f76d96de02c04943
Paul de Raaij <paul@paulderaaij.nl>
2017-06-24 15:12:59 +0200

    Adds initial ExerciseItem model

---
+++ app/DoctrineMigrations/Version20170624131107.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170624131107 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE ExerciseItem (id UUID NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE ExerciseItem');
+    }
+}
---
+++ src/HatilistBundle/Domain/Exercise/Item.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace HatilistBundle\Domain\Exercise;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @Orm\Table(name="ExerciseItem")
+ */
+class Item
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="guid")
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @var string
+     */
+    protected $id = null;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    protected $title = null;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    protected $description = null;
+
+}
